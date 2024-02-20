bda9af5176ac7f6330c5584d553a7abd562a66f3
Admin <admin@Admins-MacBook-Air.local>
2024-02-20 17:48:26 +0530

    second

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
 import java.util.*;
 class main
+{   
+    public static addition(int a,int b)
 {
+    System.out.println("Sum is: "+a+b);
+}
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
