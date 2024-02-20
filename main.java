10fde3d9944fc7c4f52ff04624c2efa628d90a96
Admin <admin@Admins-MacBook-Air.local>
2024-02-20 17:50:39 +0530

    third

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
 import java.util.*;
 class main
-{
+{ 
+    public static void subtraction(int a,int b)
+    {
+        System.out.println("Diff is: "+(a-b));
+    }
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
